3b0beb1f23295bb6c57469061459c0475eb92269
Dany Mrushi <mrushidany@gmail.com>
2024-11-20 22:35:22 +0300

    About section complete

---
+++ scripts/tailwind.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'spoqa': ['Spoqa Han Sans', 'sans-serif']
+            },
+            backgroundImage: {
+                'hero': "url('/images/bg/hero-bg.webp')",
+            }
+        },
+        colors: {
+            transparent: 'transparent',
+            'white': {
+                default: '#FFFFFF'
+            },
+            'dark': {
+                default: '#242424'
+            },
+            'orange': {
+                default: '#F58220'
+            },
+            'gray': {
+                default: '#7D7D7D'
+            }
+        }
+    }
+}
