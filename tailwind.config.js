629d8b965dfbe5bab04b4a835a4c8cac4ab595fc
Dany Mrushi <mrushidany@gmail.com>
2024-11-27 08:52:03 +0300

    Modified the readme file and added project assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,31 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'spoqa': ['Spoqa Han Sans', 'sans-serif']
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        transparent: 'transparent',
+        'white': {
+          default: '#FFFFFF',
+          100: '#ECEFF1'
+        },
+        'dark': {
+          default: '#242424',
+          100: '#2C2C2C'
+        },
+        'orange': {
+          default: '#F58220'
+        },
+        'gray': {
+          default: '#7D7D7D',
+          100: '#575757',
+          200: '#BBBBBB',
+          300: '#979797'
+        },
+        'blue': {
+          default: '#00427A'
+        }
       },
     },
   },
